5c906f80c788a69d6de5d15a7ef1b4d1387efce4
Sean Bhupathi <seanbhupathi@gmail.com>
2017-04-23 20:45:04 -0400

    random number guess generator

---
+++ number-wizard-ui/Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level start requested for: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("Level quit requested for: " + name);
+		Application.Quit();
+	}
+	
+}
---
+++ number-wizard-ui/Assets/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	
+	int max;
+	int min;
+	int guess;
+	
+	int maxGuessesAllowed = 5;
+	
+	public Text text;
+	
+	void Start () {
+		StartGame ();
+	}
+	
+	void StartGame (){
+		
+		max = 1000;
+		min = 1;
+		NextGuess();
+		
+		max = max + 1;
+	}
+	
+	public void GuessLower(){
+		max = guess;
+		NextGuess ();
+	}
+	
+	public void GuessHigher(){
+		min = guess;
+		NextGuess ();
+	}
+	
+	void NextGuess (){
+		guess = Random.Range(min, max + 1);
+		text.text = guess.ToString();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		
+		if(maxGuessesAllowed <= 0){
+			Application.LoadLevel("Win");
+		}
+	}
+}
